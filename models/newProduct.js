1889f2e39ca23c1ca86787a50fef5d5fb3dfe79d
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-09-03 16:35:51 +0200

    chore: created a crud application and tested it on postman

---
+++ models/newProduct.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const productModel = mongoose.model("Product", productSchema);
+module.exports = productModel;
--- server.js
+++ server.js
@@ -1,33 +1,70 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/newProduct");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+const connectServer = process.env.MONGODB_URI;
 
 mongoose
-  .connect(str)
-  .then((result) => console.log("connected to db: " + result))
+  .connect(connectServer)
+  .then(() => {
+    console.log("connected to db");
+  })
   .catch((error) => console.log("Captured some errors: " + error));
-app.set("view enging", "ejs");
 
-app.get("/", (req, res) => {
-  console.log("Here we are");
-  res.render("about.ejs");
+app.listen(3000, () => console.log("connected to the port 3000"));
+
+app.post("/products", async (req, res) => {
+  try {
+    const newPro = new Product(req.body);
+    const savedProduct = await newPro.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-app.get("/users", (req, res) => {
-  res.send("<h1>This is the users list</h1>");
-});
-app.get("/usersid", (req, res) => {
-  res.send("<h1>This is the users identities</h1>");
-});
-
-app.get("/:id", (req, res) => {
-  res.send(`We got the id ${req.params.id}`);
+app.get("/products", async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    res.json(allProducts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.put()
-app.post()
-app.delete()
+app.get("/products/:id", async (req, res) => {
+  const getProduct = await Product.findById(req.params.id);
+  try {
+    if (!getProduct) return res.status(404).json({ message: "Item not found" });
+    res.json(getProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.listen(3000);
+app.put("/products/:id", async (req, res) => {
+  try {
+    const updateProduct = await Product.findByIdAndUpdate(req.params.id);
+    if (!updateProduct)
+      return res.status(404).json({ message: "Item not found" });
+    res.json(updateProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deleteProduct)
+      return res.status(404).json({ message: "Item not found" });
+    res.json({ message: "Item deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
